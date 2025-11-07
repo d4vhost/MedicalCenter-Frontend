d931e2495a7a47ac84c783b7aec0476a586b6054
d4vhost <davidandradz7@gmail.com>
2025-11-07 09:16:56 -0500

    Implementacion de distribicion pasiva y fragmentacion4

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,27 @@
-// src/services/api.ts
+// Archivo: src/services/api.ts
+
 import axios from 'axios'
 
-const apiClient = axios.create({
+// 1. Crea la instancia de Axios
+const api = axios.create({
   baseURL: 'https://localhost:7188/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
 })
 
-export default apiClient
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+
+    // 4. Si el token existe, lo añade a las cabeceras
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => {
+    // Maneja errores en la configuración de la solicitud
+    return Promise.reject(error)
+  },
+)
+
+export default api
